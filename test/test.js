1d0d010e5446b093e1d87d2fea875b0e2f2d5661
John Otander <johnotander@gmail.com>
2014-11-26 09:51:30 -0700

    Add a few more tests

--- test/test.js
+++ test/test.js
@@ -19,6 +19,13 @@ describe('mac-regex', function() {
     assert.equal(mac().test('11-22-33-44-55-66'), true);
   });
 
+  it('should handle a wide array of macs', function() {
+    var validMacs = ['11:22:aa:44:55:33', '11-aa-bb-33-55-11'];
+    validMacs.forEach(function(validMac) {
+      assert.equal(mac().test(validMac), true);
+    });
+  });
+
   it('should not find a mac when it does not exist', function() {
     var invalidMacs = ['', 'aa', 'aabbccddeeff', '1234', '*&^(&^'];
     invalidMacs.forEach(function(invalidMac) {
