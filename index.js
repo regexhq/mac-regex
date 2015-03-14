34a700b2a4b10ee53b9abd6dcaaed3981fe1762a
Kevin Martensson <kevinmartensson@gmail.com>
2015-03-14 01:41:04 +0100

    Use `ignore case` flag in regex

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 module.exports = function(options) {
   options = options || {};
-  var regexBase = '([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})';
+  var regexBase = '([0-9a-f]{2}[:-]){5}([0-9a-f]{2})';
 
-  return options.exact ? new RegExp('^' + regexBase + '$') :
-                         new RegExp(regexBase, 'g');
+  return options.exact ? new RegExp('^' + regexBase + '$', 'i') :
+                         new RegExp(regexBase, 'ig');
 }
